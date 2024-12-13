48e945118984a70dd5f00851925c460cbe8a2696
Priyen-Parmar <priyen3105@gmail.com>
2024-12-13 14:25:09 +0530

    Signup Java

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    //implementation("com.intuit.sdp:sdp-android:1.1.1")
+    implementation(libs.sdpandroid)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/session/demo/MainActivity.java
+++ app/src/main/java/com/session/demo/MainActivity.java
@@ -1,18 +1,58 @@
 package com.session.demo;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button signup;
+    EditText firstName,lastName,contact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signup = findViewById(R.id.main_signup);
+
+        firstName = findViewById(R.id.main_first_name);
+        lastName = findViewById(R.id.main_last_name);
+        contact = findViewById(R.id.main_contact);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(firstName.getText().toString().trim().equals("")){
+                    firstName.setError("First Name Required");
+                }
+                else if(lastName.getText().toString().trim().equals("")){
+                    lastName.setError("Last Name Required");
+                }
+                else if(contact.getText().toString().trim().equals("")){
+                    contact.setError("Contact No. Required");
+                }
+                else if(contact.getText().toString().trim().length()<10){
+                    contact.setError("Valid Contact No. Required");
+                }
+                else{
+                    System.out.println("Signup Successfully");
+                    Log.d("RESPONSE","Signup Successfully");
+                    Log.e("RESPONSE","Signup Successfully");
+                    Log.w("RESPONSE","Signup Successfully");
+                    Toast.makeText(MainActivity.this, "Signup Successfully", Toast.LENGTH_LONG).show();
+                    Snackbar.make(v,"Signup Successfully",Snackbar.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
+
 }
