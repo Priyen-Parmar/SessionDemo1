1282b7f2cb83be1de2accb20e0635764fb24e23e
Hemil13 <157737404+Hemil13@users.noreply.github.com>
2024-12-27 12:30:34 +0530

    Recycler View list

--- app/src/main/java/com/session/demo/RecylerDemoActivity.java
+++ app/src/main/java/com/session/demo/RecylerDemoActivity.java
@@ -16,10 +16,20 @@ import java.util.ArrayList;
 
 public class RecylerDemoActivity extends AppCompatActivity {
 
-    RecyclerView recyclerView;
+    RecyclerView recyclerView, recyclerView_second, recyclerView_third;
     String[] nameArray = {"Cloth","Sports","Food","Jewelery"};
     int[] imageArray = {R.drawable.cloth,R.drawable.sports,R.drawable.food,R.drawable.jewelery};
     ArrayList<CustomList> arrayList;
+
+
+
+    String[] itemArray2 = {"Camera","Compass","Airplane","Map","Camping","Luggage", "Cloth", "Food"};
+    int[] ImgArray2 = {R.drawable.camera,R.drawable.compass,R.drawable.airplane, R.drawable.map, R.drawable.camping, R.drawable.luggage,R.drawable.cloth,R.drawable.food};
+    ArrayList<CustomList> arrayList2;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +42,7 @@ public class RecylerDemoActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new StaggeredGridLayoutManager(1,StaggeredGridLayoutManager.HORIZONTAL));
         recyclerView.setItemAnimator(new DefaultItemAnimator());
 
-        //recyclerView.setNestedScrollingEnabled(false);
+        recyclerView.setNestedScrollingEnabled(false);
 
         arrayList = new ArrayList<>();
         for(int i=0;i< nameArray.length;i++){
@@ -43,5 +53,34 @@ public class RecylerDemoActivity extends AppCompatActivity {
         }
         RecyclerDemoAdapter adapter = new RecyclerDemoAdapter(RecylerDemoActivity.this,arrayList);
         recyclerView.setAdapter(adapter);
+
+
+
+        // Second Recycler View
+        recyclerView_second = findViewById(R.id.recyclerview_second);
+        recyclerView_second.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.HORIZONTAL));
+
+        arrayList2 = new ArrayList<>();
+        for(int i=0; i < itemArray2.length; i++){
+            CustomList list = new CustomList();
+            list.setCatImage(ImgArray2[i]);
+            list.setCatName(itemArray2[i]);
+            arrayList2.add(list);
+        }
+
+        RecyclerDemoAdapter adapter2 = new RecyclerDemoAdapter(RecylerDemoActivity.this, arrayList2);
+        recyclerView_second.setAdapter(adapter2);
+
+
+
+
+        // Third RecyclerView
+        recyclerView_third = findViewById(R.id.recyclerview_third);
+        recyclerView_third.setLayoutManager(new LinearLayoutManager(RecylerDemoActivity.this));
+
+        RecyclerDemoAdapter adapter3 = new RecyclerDemoAdapter(this, arrayList2);
+        recyclerView_third.setAdapter(adapter3);
+
+
     }
 }
